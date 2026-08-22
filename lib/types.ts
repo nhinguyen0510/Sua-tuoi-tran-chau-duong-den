0e977d682aaf3d1e798aafaab7f9f58ec5409c94
nhinguyen0510 <nhi.nguyen1@hrdigest.org>
2026-08-22 22:30:09 +0700

    Create types.ts

---
+++ lib/types.ts
@@ -0,0 +1,24 @@
+export type InterviewQuestion = {
+  question: string;
+  why_it_matters: string;
+  suggested_answer_points: string[];
+};
+
+export type ActionWeek = {
+  week: string;
+  goals: string[];
+};
+
+export type AnalysisResult = {
+  candidate_summary: string;
+  target_role: string;
+  match_score: number;
+  strengths: string[];
+  missing_skills: string[];
+  transferable_skills: string[];
+  australian_resume_summary: string;
+  rewritten_bullets: string[];
+  interview_questions: InterviewQuestion[];
+  action_plan_30_days: ActionWeek[];
+  responsible_ai_notes: string[];
+};
